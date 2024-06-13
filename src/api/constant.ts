36952ccbc98d3e7870b035906b3554327a19d0df
shantanu16 <shantanubhusari16@gmail.com>
2024-06-13 10:01:29 +0530

    API Token added

--- src/api/constant.ts
+++ src/api/constant.ts
@@ -1,5 +1,5 @@
-export const API_TOKEN = "<ADD YOUR TOKEN>"
-  
+export const API_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzYyZTVkODdkMWU0MmZhMzgxYjJhYjc2MjA5MGIxYiIsInN1YiI6IjYyMDM1YWVlNDZmMzU0MDA0MjBhNTdkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NHzob3_iR8YG2Yi-f1O_i8kiFN-mSl5RWgH8PyEo9t8";
 
 export const BASE_URL = "https://api.themoviedb.org/3";
 
